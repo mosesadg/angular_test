32f9ca5bfc417ee8fb2503c16baad51e67d2f9bc
Anand Moses <moses.adg@gmail.com>
2016-06-16 15:07:34 -0400

    Angular 2 test

---
+++ app/auto-grow.directive.js
@@ -0,0 +1,23 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var AutoGrowDirective;
+    return {
+        setters:[],
+        execute: function() {
+            {
+                selector: '[autoGrow]',
+                    host;
+                {
+                }
+            }
+            AutoGrowDirective = (function () {
+                function AutoGrowDirective() {
+                }
+                return AutoGrowDirective;
+            }());
+            exports_1("AutoGrowDirective", AutoGrowDirective);
+        }
+    }
+});
+//# sourceMappingURL=auto-grow.directive.js.map
---
+++ app/auto-grow.directive.ts
@@ -0,0 +1,12 @@
+import {Directive} from 'angular2/core'
+
+@Directive{
+    selector: '[autoGrow]',
+    host: {
+        
+    }
+}
+
+export class AutoGrowDirective{
+
+}
